5ddf5a0ad8a07989e30781fc078257294dfb8b69
Balazs Sos <sos.balazs@gmail.com>
2022-02-13 17:30:18 +0100

    initial commit

---
+++ src/A.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "A.h"
+
+void A::hello() {
+  std::cout << "Hello World!" << std::endl;
+}
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "A.h"
+
+*int main() {
+  A a;
+  a.hello();
+  return 0; 
+}
+
